6f7a970fc1c41654662149cb7369d05397c064fc
venkatmaredla <93971254+venkatmaredla@users.noreply.github.com>
2022-02-06 10:17:24 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 
 
   
-const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service on k8 s";
+const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service ";
 const portMessage = "Application Running on port";
 
 const getWelcomeMessage = () => {
